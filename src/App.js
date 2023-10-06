ad074492b56c40b979eaf863d2bd6bf8eb4af84e
Hilmatrix <hilmaul@gmail.com>
2023-10-06 07:04:41 +0700

    adding signup and login component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from './Components/Landing_Page/Landing_Page';
+import Login from './Components/Login/Login';
 import Navbar from './Components/Navbar/Navbar';
+import Sign_up from './Components/Sign_up/Sign_up';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
           <Navbar/>
               <Routes>
                 <Route path="/" element={<LandingPage/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/signup" element={<Sign_up/>}/>
               </Routes>
             
         </BrowserRouter>
